6e15e78ee6b4d837108dd0d9a9cf975fd91a3694
Rinat Gabidullin <gabidullin_rg@mail.ru>
2022-01-16 10:38:42 +0500

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const { src, dest, parallel } = require('gulp');
+const pug = require('gulp-pug');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+const sass = require('gulp-sass')(require('sass'));
+
+const sassCompile = () => {
+  console.log('Компиляция SASS и установка префиксов');
+  return src('app/scss/style.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(dest('build/'));
+};
+
+const pugCompile = () => {
+  console.log('Компиляция PUG');
+  return src('app/html/index.pug')
+    .pipe(pug())
+    .pipe(dest('build/'));
+};
+
+const minifyCss = () => {
+  console.log('Минимизация CSS');
+  return src('build/style.css')
+    .pipe(cleanCss())
+    .pipe(dest('build/'));
+};
+
+exports.default = parallel(sassCompile, pugCompile);
+exports.sassCompile = sassCompile;
+exports.pugCompile = pugCompile;
+exports.minifyCss = minifyCss;
